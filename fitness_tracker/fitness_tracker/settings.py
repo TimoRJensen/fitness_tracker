e7c85d2267693daa070e566c05e76a4780ba8666
TimoRJensen <ich@timo-jensen.de>
2024-09-15 21:54:45 +0200

    first green - added user model

--- fitness_tracker/fitness_tracker/settings.py
+++ fitness_tracker/fitness_tracker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    ###
+    "tracker",
 ]
 
 MIDDLEWARE = [
@@ -47,8 +49,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    ###
-    "tracker",
 ]
 
 ROOT_URLCONF = "fitness_tracker.urls"
@@ -126,3 +126,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Added for the project
+AUTH_USER_MODEL = "tracker.User"
--- fitness_tracker/tracker/tests.py
+++ fitness_tracker/tracker/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+# tracker/tests/test_models.py
 
-# Create your tests here.
+import pytest
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@pytest.mark.django_db
+def test_user_creation():
+    user = User.objects.create_user(
+        username="testuser", password="testpass123", email="testuser@example.com"
+    )
+    assert user.username == "testuser"
+    assert user.email == "testuser@example.com"
+    assert user.check_password("testpass123")
